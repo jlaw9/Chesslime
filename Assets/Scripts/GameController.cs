32cbf8d2bc3a308845b60b70a28e3c68ed69cefe
Jeff Law <jeffreynlaw@gmail.com>
2016-11-09 22:33:33 -0500

    Added all of the Unity Files
    There are probably a bunch we don't need to keep. I'm just syncing
    everything for now.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,415 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class GameController: MonoBehaviour {
+
+    int masterRows = 4;  // number of rows in the master matrix
+    int masterCols = 5;  // number of columns in the master matrix
+    int totalSquare;
+    // matrix of pointers to the game board squares list
+    // c# doesn't use pointers, so this is the next best thing I think
+    public int[,] squaresMatrix;
+    public GameSquares[] squaresList;  // container of all of the game board squares
+    public List<List<int>> prevSquaresList;  // contains the player and slime of each square from the previous turn
+    int currentPlayer; //this shows if it’s player 1 or player 2’s turn.
+    int prevPlayer; //this shows if it’s player 1 or player 2’s turn.
+    int currentTurn; //how many turns since the game started.
+    int rows;  // number of rows specified by the user
+    int cols;  // number of columns specified by the user
+    List<int> players;  // list of players
+    Dictionary<int, Color32> playerColors = new Dictionary<int, Color32>();  // dictionary of colors for players
+
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+    public GameObject resetButton;
+    public GameObject startGame;
+    public Dropdown selectRows;
+    public Dropdown selectCols;
+    public Dropdown selectPlayers;
+    public GameObject gameBoardBackground;
+    public GameObject undoButton;
+
+
+    void Awake()
+    {
+        squaresMatrix = new int[masterRows, masterCols];
+        prevSquaresList = new List<List<int>>();
+        SetGameControllerReferenceOnButtons();
+        //playerColors[1] = new Color32(255,0,102,100);
+        //playerColors[2] = new Color32(0, 204, 204, 100);
+        //playerColors[3] = new Color32(104, 204, 50, 100);
+        playerColors[0] = new Color32(0, 0, 0, 100);
+        playerColors[1] = Color.red;
+        playerColors[2] = Color.blue;
+        playerColors[3] = Color.green;
+        playerColors[4] = Color.magenta;
+        playerColors[5] = Color.cyan;
+        playerColors[6] = Color.yellow;
+
+        initializeBoard();
+        SelectOptions();
+    }
+
+
+    // links it to the square pieces in Unity
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < squaresList.Length; i++)
+        {
+            squaresList[i].SetGameControllerReference(this);
+            prevSquaresList.Add(new List<int>());
+            prevSquaresList[i].Add(0);
+            prevSquaresList[i].Add(0);
+        }
+    }
+
+
+    public void SelectOptions()
+    {
+        // Set selection options to true
+        selectRows.gameObject.SetActive(true);
+        selectCols.gameObject.SetActive(true);
+        selectPlayers.gameObject.SetActive(true);
+        startGame.SetActive(true);
+
+        // set game over text to inactive
+        gameOverPanel.SetActive(false);
+        resetButton.SetActive(false);
+        undoButton.SetActive(false);
+        gameBoardBackground.SetActive(false);
+        for (int i = 0; i < squaresList.Length; i++)
+        {
+            squaresList[i].gameObject.SetActive(false);
+            squaresList[i].neighbors = new List<int>();
+        }
+    }
+
+
+    public void StartGame()
+    {
+        selectRows.gameObject.SetActive(false);
+        selectCols.gameObject.SetActive(false);
+        selectPlayers.gameObject.SetActive(false);
+        startGame.SetActive(false);
+        gameBoardBackground.SetActive(true);
+        undoButton.SetActive(true);
+        undoButton.GetComponent<Button>().interactable = false;
+
+        // these should be selected by the user
+        rows = int.Parse(selectRows.captionText.text);
+        cols = int.Parse(selectCols.captionText.text);
+        int num_players = int.Parse(selectPlayers.captionText.text);
+        Debug.Log(rows.ToString() +","+ cols.ToString());
+
+        // number of players
+        players = new List<int>();
+        for (int i = 1; i <= num_players; i++)
+        {
+            players.Add(i);
+        }
+
+        BuildBoard(rows, cols);
+
+        currentPlayer = 1;
+        currentTurn = 1;
+    }
+
+
+    public void SetBoardBackground(int rows, int cols)
+    {
+        // set the size of the game board background
+        gameBoardBackground.GetComponent<RectTransform>().sizeDelta = new Vector2(50 * cols + 10, 50 * rows + 10);
+
+        // set the coordinates
+        // middle x coord of the squares 
+        float middleXCoord = 0;
+        float middleYCoord = 0;
+        for (int col = 0; col < cols; col++)
+        {
+            middleXCoord += squaresList[squaresMatrix[0, col]].transform.position.x;
+        }
+        for (int row = 0; row < rows; row++)
+        {
+            middleYCoord += squaresList[squaresMatrix[row, 0]].transform.position.y;
+        }
+        // divide by the number of columns to get the average x coordinate
+        middleXCoord = middleXCoord / cols;
+
+        middleYCoord = middleYCoord / rows;
+        // move it to the center of the active rows and columns
+        gameBoardBackground.transform.position = new Vector3(middleXCoord, middleYCoord, gameBoardBackground.transform.position.z);
+
+    }
+
+
+    void initializeBoard()
+    {
+        // add the index of the square in the squaresList to the matrix
+        int index = 0;
+        for (int row = 0; row < masterRows; row++)
+        {
+            for (int col = 0; col < masterCols; col++)
+            {
+                squaresMatrix[row, col] = index;
+                index++;
+            }
+        }
+    }
+
+
+    // Initializes the squares with their neighbors and limit
+    void BuildBoard(int rows, int cols)
+    {
+        SetBoardBackground(rows, cols);
+        int index;
+        // add the limit and neighbors to each square
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                Debug.Log("Adding " + row + "," + col);
+                index = squaresMatrix[row, col];
+                squaresList[index].gameObject.SetActive(true);
+                //Debug.Log("Testing index: " + index.ToString() + ", row: " + row.ToString() + ", col: " + col.ToString());
+                if (row - 1 >= 0)
+                {
+                    squaresList[index].neighbors.Add(squaresMatrix[row - 1, col]);  // add the left neighbor
+                }
+                if (row + 1 < rows)
+                {
+                    squaresList[index].neighbors.Add(squaresMatrix[row + 1, col]);  // add the left neighbor
+                }
+                if (col - 1 >= 0)
+                { 
+                    squaresList[index].neighbors.Add(squaresMatrix[row, col - 1]);  // add the left neighbor
+                }
+                if (col + 1 < cols)
+                {
+                    squaresList[index].neighbors.Add(squaresMatrix[row, col + 1]);  // add the left neighbor
+                }
+                    // set the limit of the square equal to the number of neighbors
+                squaresList[index].limit = squaresList[index].neighbors.Count;
+            }
+        }
+        /* 
+        // inactivate any unused squares
+        // check by rows and then by columns. If the user selects the same number of rows as the master, then the first for loop won't start.
+        for (int row = 0; row < masterRows; row++)
+        {
+            for (int col = 0; col < masterCols; col++)
+            {
+                if (row >= rows || col >= cols)
+                {
+                    Debug.Log("Inactivating " + row + "," + col);
+                    squaresList[squaresMatrix[row, col]].gameObject.SetActive(false);
+                }
+            }
+        }
+        */
+    }
+
+
+    // check if there is a winner and move to the next player's turn.
+    public void EndTurn()
+    {
+        //Debug.Log("EndTurn is not implemented!");
+        // check for explosions
+        while (checkExplosions())
+        {
+            // check for a winner after each explosion
+            if (checkWin())
+            {
+                GameOver();
+                // if there was a winner, break out of the explosion loop
+                break;
+            }
+        }
+        prevPlayer = currentPlayer;
+        // also activate the undo button
+        undoButton.GetComponent<Button>().interactable = true;
+        
+        NextPlayer();
+        currentTurn++;
+
+
+    }
+
+
+    // checks the board to see if there are any explosions. If there are, it explodes the square.
+    // returns true if at least one square exploded. Otherwise returns false
+    bool checkExplosions()
+    {
+        bool explosion = false;
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                int index = squaresMatrix[row, col];
+                if (squaresList[index].current_slime >= squaresList[index].limit)
+                {
+                    // explode this square
+                    explosion = true;
+                    ExplodeSquare(index);
+                }
+            }
+        }
+        return explosion;
+    }
+
+
+    // set slime of square to 0, and take over and add slime to the square's neighbors
+    void ExplodeSquare(int squareIndex)
+    {
+        squaresList[squareIndex].SetSlime(0);
+        foreach (int neighbor in squaresList[squareIndex].neighbors)
+        {
+            squaresList[neighbor].AddSlime(takeover:true);
+        }
+    }
+
+
+    //checks the board to see if player on is the only one left on the board or if player to is the only one left on the board.Breaks out of the loop if the player is found
+    bool checkWin()
+    {
+        HashSet<int> currentPlayers = new HashSet<int>();
+        for (int i = 0; i < squaresList.Length; i++)
+        {
+            currentPlayers.Add(squaresList[i].player);
+        }
+        // remove the null player
+        currentPlayers.Remove(0);
+
+        // remove any players that aren't on the board anymore
+        List<int> newPlayersList = new List<int>();
+        foreach (int player in players)
+        {
+            if (!currentPlayers.Contains(player))
+            {
+                Debug.Log("Player " + player + " DEFEATED");
+            }
+            else
+            {
+                newPlayersList.Add(player);
+            }
+        }
+        players = newPlayersList;
+
+        if (currentPlayers.Count == 1)
+        {
+            //Debug.Log("Player " + currentPlayer.ToString() + " wins!");
+            // only one player left, so we have a winner!
+            return true;
+        }
+        // No winner yet.
+        return false;
+    }
+
+
+    void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        // the player who took the last turn won
+        gameOverText.text = "Player " + currentPlayer.ToString() + " Wins!";
+        SetBoardInteractable(toggle: false);
+        // give the option to play again
+        resetButton.SetActive(true);
+        undoButton.SetActive(false);
+    }
+
+
+    public void SetBoardInteractable(bool toggle=false)
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                squaresList[squaresMatrix[row, col]].button.interactable = toggle;
+            }
+        }
+    }
+
+
+    public void RestartGame()
+    {
+        SetBoardInteractable(toggle: true);
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                // reset every square to 0 slime
+                squaresList[squaresMatrix[row, col]].SetSlime(0);
+            }
+        }
+        SelectOptions();
+    }
+
+    public void Undo()
+    {
+        undoButton.GetComponent<Button>().interactable = false;
+        //squaresList = CopySquareStatus(prevSquaresList, squaresList);
+        for (int i = 0; i < squaresList.Length; i++)
+        {
+            //Debug.Log("Setting slime and player of square " + i + " to " + prevSquaresList[i][0] + " " + prevSquaresList[i][1]);
+            if (squaresList[i].player != prevSquaresList[i][0])
+            {
+                squaresList[i].SetPlayer(prevSquaresList[i][0]);
+            }
+            if (squaresList[i].current_slime != prevSquaresList[i][1])
+            {
+                squaresList[i].SetSlime(prevSquaresList[i][1]);
+            }
+        }
+        currentPlayer = prevPlayer;
+        // TODO Undo doesn't work for players being eliminated yet.
+        //players = prevPlayers;
+        currentTurn--;
+    }
+
+    // copy the slime and player status from list1 to list2
+    public GameSquares[] CopySquareStatus(GameSquares[] list1, GameSquares[] list2)
+    {
+        for (int i = 0; i < list1.Length; i++)
+        {
+            Debug.Log("Setting slime and player of square " + i + " to " + list1[i].current_slime + " " + list1[i].player);
+            list2[i].SetSlime(list1[i].current_slime);
+            list2[i].SetPlayer(list1[i].player);
+        }
+        return list2;
+    }
+
+
+    // Next player's turn
+    void NextPlayer()
+    {
+        // TODO set up to allow more players
+        //currentPlayer = (currentPlayer == 1) ? 2 : 1;
+        int curr_player_index = players.IndexOf(currentPlayer);
+        int next_player_index = (curr_player_index + 1) % players.Count;
+        currentPlayer = players[next_player_index];
+        /*
+        def next_player(self):
+        #print "curr player was:", self.curr_player
+        #next_player_index = ((self.curr_player) % len(self.players))
+        curr_player_index = self.players.index(self.curr_player)
+        next_player_index = (curr_player_index + 1) % len(self.players)
+        self.curr_player = self.players[next_player_index]
+        #print "curr player is:", self.curr_player
+        return self.curr_player
+        */
+    }
+
+
+    public int GetPlayerSide()
+    {
+        return currentPlayer;
+    }
+
+    public Color32 GetPlayerColor(int player)
+    {
+        return playerColors[player];
+    }
+
+}
---
+++ Assets/Scripts/GameSquares.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameSquares : MonoBehaviour {
+
+    public GameObject Square;
+    public int player = 0; // What player owns this square, 0 for neither, 1 for player 1, and 2 for player 2.
+    public int limit; //type of square/limit of pieces before it's set to explode
+    public int current_slime = 0;  // amount of slime currently in this square
+    public Button button;
+    public Text squareText;  // text to display
+    //int squarePos; //where the square is in the board array.
+    public List<int> neighbors;  // the neighboring squares of this square
+    private GameController gameController;
+
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+
+    public void TakeTurn()
+    {
+        // get the game square status before this turn was taken so the user can undo.
+        //gameController.prevSquaresList = gameController.CopySquareStatus(gameController.squaresList, gameController.prevSquaresList);
+        for (int i = 0; i < gameController.squaresList.Length; i++)
+        {
+            gameController.prevSquaresList[i][0] = gameController.squaresList[i].player;
+            gameController.prevSquaresList[i][1] = gameController.squaresList[i].current_slime;
+        }
+
+        bool added = AddSlime(takeover:false);
+        if (added)
+        {
+            gameController.EndTurn();
+        }
+        else
+        {
+            // don't end the turn until the player makes a legal move.
+            Debug.Log("Try again");
+        }
+    }
+
+
+    public bool AddSlime(bool takeover=false)
+    {
+        int current_player = gameController.GetPlayerSide();
+        if (player == 0 || takeover)
+        {
+            SetPlayer(current_player);
+        }
+        if (current_player != player)
+        {
+            // illegal move
+            Debug.Log("square owned by: " + this.player);
+            return false;
+        }
+        else
+        {
+            SetSlime(current_slime + 1);
+            return true;
+        }
+    }
+
+
+    public void SetPlayer(int player)
+    {
+        this.player = player;
+        // set the color of the text to be the player's color.
+        squareText.color = gameController.GetPlayerColor(player);
+    }
+
+
+    public void SetSlime(int slime)
+    {
+        current_slime = slime;
+        // TODO this should be replaced with slime sprites rather than text
+        squareText.text = current_slime.ToString();
+
+        if (current_slime == 0)
+        {
+            SetPlayer(0);
+        }
+    }
+
+}
